fdb6006b389e2836909a41454a8acda8859c23cb
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-21 21:33:28 +0200

    Add files via upload

---
+++ Task01_National_Court/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task01_National_Court
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int timeToFinish = 0;
+            int peoplesPerHour = 0;
+            for (int i = 1; i <= 3; i++)
+            {
+                peoplesPerHour += int.Parse(Console.ReadLine());
+            }
+
+            int people = int.Parse(Console.ReadLine());
+
+            while (people > 0)
+            {
+                timeToFinish += 1;
+                people -= peoplesPerHour;
+                if(timeToFinish % 4 == 0)
+                {
+                    timeToFinish += 1;
+                }
+            }
+            
+
+            Console.WriteLine($"Time needed: {timeToFinish}h.");
+        }
+    }
+}
